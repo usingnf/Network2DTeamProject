ccadec957a59d7892b9b725ac81a85a66a608a02
LeeInSeok <50188137+usingnf@users.noreply.github.com>
2022-06-16 19:57:08 +0900

    Player basic Standard
    Create basic Player Move

---
+++ Assets/LeeInSeok/Scripts/IPushable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PushDir
+{
+    Left = -1,
+    Right = 1,
+}
+
+public interface IPushable
+{
+    void Push(PushDir dir, float power);
+}
