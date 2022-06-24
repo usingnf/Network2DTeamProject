228e263d90a528525e8389d57fd4d89f021ebb6a
LeeInSeok <50188137+usingnf@users.noreply.github.com>
2022-06-24 21:32:57 +0900

    Lobby Option Add
    Lobby Option Add

--- Assets/Common/Manager/LobbyManager.cs
+++ Assets/Common/Manager/LobbyManager.cs
@@ -17,7 +17,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public InLobbyPanel inLobbyPanel;
     public InRoomPanel inRoomPanel;
     public InfoPanel infoPanel;
-    public OptionPanel optionPanel;
+    public GameObject optionPanel;
 
     #region UNITY
 
@@ -41,7 +41,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         createRoomPanel.gameObject.SetActive(panel == PANEL.CreateRoom);
         inLobbyPanel.gameObject.SetActive(panel == PANEL.Lobby);
         inRoomPanel.gameObject.SetActive(panel == PANEL.Room);
-       // optionPanel.gameObject.SetActive(panel == PANEL.Option);
+        optionPanel.gameObject.SetActive(panel == PANEL.Option);
     }
 
     public void ShowError(string error)
