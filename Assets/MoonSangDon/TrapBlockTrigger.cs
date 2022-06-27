730a49bb53a276998a442abc2b8d22c43127a3df
MoonSangDon <moon3225@naver.com>
2022-06-27 17:15:51 +0900

    commit

--- Assets/MoonSangDon/TrapBlockTrigger.cs
+++ Assets/MoonSangDon/TrapBlockTrigger.cs
@@ -5,8 +5,8 @@ using Photon.Pun;
 
 public class TrapBlockTrigger : MonoBehaviourPun
 {
-    public GameObject            box;
-    public Collider2D           trap; 
+    public GameObject       box;
+    public Collider2D        trap; 
     private SpriteRenderer      rend;
     private Color               invisibleColor;
     private Color               originColor;
@@ -17,7 +17,6 @@ public class TrapBlockTrigger : MonoBehaviourPun
     void Start()
     { 
         rend =              GetComponentInChildren<SpriteRenderer>();
-        box.SetActive(false);
         //originColor =       rend.color;
         //invisibleColor.a =  0f;
         //rend.color =        invisibleColor;
@@ -29,7 +28,6 @@ public class TrapBlockTrigger : MonoBehaviourPun
         {
             Debug.Log("엔터엔터됨");
             Debug.Log("트랩블럭콜라이더 엔터됨");
-            OnActiveBlock();
             photonView.RPC("OnActiveBlock", RpcTarget.All);
         }
     }
