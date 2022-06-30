d624b298b11ae977230c46d0e1bb7f5df98d1d9f
MoonSangDon <moon3225@naver.com>
2022-06-30 20:29:45 +0900

    LobbyBGM,StageBGM,BGM Controler Add

--- Assets/SeoHunGyo/Scripts/OptionUILobby.cs
+++ Assets/SeoHunGyo/Scripts/OptionUILobby.cs
@@ -5,15 +5,29 @@ using UnityEngine.UI;
 
 public class OptionUILobby : MonoBehaviour
 {
+    public AudioSource bgm_Source;
+    public Slider bgm_Slider;
+    public GameObject bgm_GameObject;
+
     public GameObject optionUI;
     public Slider slider;
 
+    private void Start()
+    {
+        bgm_Source = bgm_GameObject.GetComponent<AudioSource>();
+        bgm_Source.volume = 0.3f;
+        bgm_Slider.value = bgm_Source.volume;
+    }
 
     private void OnEnable() 
     {
         slider.value = PlayerPrefs.GetFloat("Volume");
     }
-
+    
+    public void SetBgmVolume()
+    {
+        bgm_Source.volume = bgm_Slider.value;
+    }
 
     public void SetVolume()
     {
